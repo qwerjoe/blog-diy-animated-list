38a842a494470475d14a5b10885c202cc2daaebc
qwerjoe <qwerjoe@gmail.com>
2025-02-28 20:02:39 +0100

    diy List: add exit animation and AnimPresence wrapper

--- src/List/diy/List.tsx
+++ src/List/diy/List.tsx
@@ -1,9 +1,13 @@
 import { ListItemId, ListProps } from "@/List/ListProps";
-import { AnimDiv, AnimationProps } from "@/diy";
+import { AnimDiv, AnimPresence, AnimationProps } from "@/diy";
 import "@/List/list.css";
 
 const kInitialState: AnimationProps = { opacity: 0 };
 const kTargetState: AnimationProps = { opacity: 1, options: { duration: 300 } };
+const kExitState: AnimationProps = {
+  opacity: 0,
+  options: { duration: 300, easing: "ease-out" },
+};
 
 export function List<IdType extends ListItemId>({
   ids,
@@ -11,16 +15,19 @@ export function List<IdType extends ListItemId>({
 }: ListProps<IdType>) {
   return (
     <div className="list-root">
-      {ids.map((id) => (
-        <AnimDiv
-          initial={kInitialState}
-          animate={kTargetState}
-          options={{ duration: 200 }}
-          key={id}
-        >
-          {renderItem(id)}
-        </AnimDiv>
-      ))}
+      <AnimPresence>
+        {ids.map((id) => (
+          <AnimDiv
+            initial={kInitialState}
+            animate={kTargetState}
+            exit={kExitState}
+            options={{ duration: 200 }}
+            key={id}
+          >
+            {renderItem(id)}
+          </AnimDiv>
+        ))}
+      </AnimPresence>
     </div>
   );
 }
