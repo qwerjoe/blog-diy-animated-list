8a8622c12c8fd90d6eb81e9c3182de33097665af
qwerjoe <qwerjoe@gmail.com>
2025-02-28 19:57:42 +0100

    AnimDiv: enter/animate/options props

--- src/diy/index.ts
+++ src/diy/index.ts
@@ -1,3 +1,4 @@
 export { AnimDiv } from "./AnimDiv";
 
-export type { AnimDivProps } from "./AnimDiv";
+export type { AnimDivProps, Animation as AnimationProps } from "./AnimDiv";
+export type { AnimationEasing, AnimationOptions } from "./types";
