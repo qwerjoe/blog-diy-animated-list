691d41a82c68b1ac4ebdc3d1d4c76e437cffa009
qwerjoe <qwerjoe@gmail.com>
2025-02-28 19:57:42 +0100

    AnimDiv: enter/animate/options props

--- src/diy/AnimDiv.tsx
+++ src/diy/AnimDiv.tsx
@@ -1,25 +1,51 @@
 import { ComponentProps, useLayoutEffect, useRef } from "react";
 import { AnimNode } from "./AnimNode";
+import { Properties, AnimationOptions } from "./types";
 
-export type AnimDivProps = ComponentProps<"div">;
+export type Animation = Partial<Properties> & {
+  options?: AnimationOptions;
+};
 
-export function AnimDiv({ ...divProps }: AnimDivProps) {
-  const { domRef } = useAnimNode();
+type AnimDivOwnProps = {
+  initial?: Animation;
+  animate?: Animation;
+  options?: AnimationOptions;
+};
+
+export type AnimDivProps = ComponentProps<"div"> & AnimDivOwnProps;
+
+export function AnimDiv({
+  initial,
+  animate,
+  options,
+  ...divProps
+}: AnimDivProps) {
+  const { domRef } = useAnimNode({ initial, animate, options });
   return <div {...divProps} ref={domRef} />;
 }
 
-function useAnimNode() {
+type UseAnimNodeParams = AnimDivOwnProps;
+
+function useAnimNode({ initial, animate, options }: UseAnimNodeParams) {
   const animNodeRef = useRef<AnimNode>(new AnimNode());
   const animNode = animNodeRef.current;
 
   animNode.beforeUpdate();
 
   useLayoutEffect(() => {
-    animNode.mount();
+    animNode.mount(initial);
     return () => {
       animNode.unmount();
     };
   }, []);
+
+  useLayoutEffect(() => {
+    animNode.setDefaultOptions(options ?? {});
+  }, [options]);
+
+  useLayoutEffect(() => {
+    animNode.animateTo(animate ?? {}, animate?.options);
+  }, [animate]);
 
   useLayoutEffect(() => {
     animNode.afterUpdate();
